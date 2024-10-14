64af086422ee87cef9ee6cbff935206c6a14eff2
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-10-14 22:28:41 +0000

    ideia

---
+++ fifo.py
@@ -0,0 +1,4 @@
+# precisa receber espaços das pages e a lista de valores separados por vírgula
+# Precisa criar os espaços de acordo com os espaços das pages dados pelo usuário 
+# e colocar todos os valores como -1
+# Precisa separar os números que estão separados por vírugula em uma lista
