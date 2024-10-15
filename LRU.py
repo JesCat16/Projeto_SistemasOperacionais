5da5c9dbb6ad899231502840248fd2c19ccaf432
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-10-15 01:26:48 +0000

    lru

--- LRU.py
+++ LRU.py
@@ -24,42 +24,35 @@ for i in range(len(lis)):
 
             else:
                 print(f'[{lista[z]}] <- (hit)')
-        print(lista2)
 
     else: #se der miss
         miss= miss + 1
-        if lis[i] not in lista2:
-            lista2[cont2] = lis[i]
-            if cont2 == tam-2:
-                cont2 = 0
-            else:
-                cont2 += 1
-            print(cont2)
+        # if lis[i] not in lista2:
+        #     lista2[cont2] = lis[i]
+        #     if cont2 == tam-2:
+        #         cont2 = 0
+        #     else:
+        #         cont2 += 1
         for z in range(tam):
         #     if lis[z] in lista2:
         #        print(f'[{lista[z]}]')
         #     else:
         #        lista[z] = lis[i]
         #        print(f'[{lista[z]}] <- (miss)')
-            
-            if lis[z] not in lista2 and controle == 0:
+            # print(lis[z], lista2)
+            if lis[i] not in lista2 and controle == 0:
                 lista[z] = lis[i]
                 print(f'[{lista[z]}] <- (miss)')
                 controle += 1
             else:
                 print(f'[{lista[z]}]')
         
-        #if lis[i] not in lista2:
-        #    lista2[cont2] = lis[i]
-        #    if cont2 == tam-2:
-        #        cont2 = 0
-        #    else:
-        #        cont2 += 1
-        print(lista2)
-        if cont == tam-1:
-            cont = 0
-        else:
-            cont += 1
+        if lis[i] not in lista2:
+           lista2[cont2] = lis[i]
+           if cont2 == tam-2:
+               cont2 = 0
+           else:
+               cont2 += 1
         controle = 0
 print(f'Hit rate: {hit}/{len(lis)}')
 print(f'Miss rate: {miss}/{len(lis)}')
