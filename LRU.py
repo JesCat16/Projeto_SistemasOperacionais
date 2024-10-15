0e6801e2bfde78fcacada86f48726fc3d1cae569
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-10-15 00:33:21 +0000

    criação do fifo

--- LRU.py
+++ LRU.py
@@ -1,34 +1,44 @@
-# precisa receber espaços das pages e a lista de valores separados por vírgula OK, uso de sys
-# Precisa criar os espaços de acordo com os espaços das pages dados pelo usuário 
-# e colocar todos os valores como -1 OK, uso do split
-# Precisa separar os números que estão separados por vírugula em uma lista ok, .split
 import sys
 hit = 0
 miss = 0
 cont = 0
+cont2 = 0
 tam = int("\n".join(sys.argv[1])) #dá set no tamanho das pages
 lista = [-1] * tam #cria a lista de pages vazias
+lista2 = [-1] * (tam-1) #cria a lista de pages vazias
 lis = list(map(int,sys.argv[2].split(','))) #cria a lista das paginas dos processos
 for i in range(len(lis)):
     print(f'pages: {lis[i]}')
     if lis[i] in lista: #se der hit
         hit = hit + 1
+        if lista2[cont2] == lis[i]:
+            if cont2 == tam-2:
+                cont2 = 0
+            else:
+                cont2 += 1
         for z in range(tam):
             if lis[i] != lista[z]:
                 print(f'[{lista[z]}]')
 
             else:
                 print(f'[{lista[z]}] <- (hit)')
+        print(lista2)
 
     else: #se der miss
         miss= miss + 1
+        if lis[i] not in lista2:
+            lista2[cont2] = lis[i]
+            if cont2 == tam-2:
+                cont2 = 0
+            else:
+                cont2 += 1
         for z in range(tam):
             if z != cont:
                 print(f'[{lista[z]}]')
             else:
                 lista[z] = lis[i]
                 print(f'[{lista[z]}] <- (miss)')
-                
+        print(lista2)
         if cont == tam-1:
             cont = 0
         else:
