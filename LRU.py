243c700c0f04913b0cf1f21a7f77cbcbb1dc67d5
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-10-15 13:44:45 +0000

    LRU

--- LRU.py
+++ LRU.py
@@ -1,58 +1,41 @@
 import sys
+
 hit = 0
 miss = 0
 cont = 0
 cont2 = 0
 controle = 0
-tam = int("\n".join(sys.argv[1])) #dá set no tamanho das pages
-lista = [-1] * tam #cria a lista de pages vazias
-lista2 = [-1] * (tam-1) #cria a lista de pages vazias
-lis = list(map(int,sys.argv[2].split(','))) #cria a lista das paginas dos processos
+tam = int("\n".join(sys.argv[1]))  # pega o tamanho da memória
+lista = [-1] * tam  # cria a lista vazia
+lista2 = [-1] * tam  # cira uma lista vazia identica para guardar as informações de prioridade
+lis = list(map(int, sys.argv[2].split(',')))  # cria uma lista com os números dados
+
 for i in range(len(lis)):
-    print(f'pages: {lis[i]}')
-    if lis[i] in lista: #se der hit
-        hit = hit + 1
-        if lis[i] in lista2:
-            if lis[i] == lista2[cont2]:
-                if cont2 == tam-2:
-                    cont2 = 0
-                else:
-                    cont2 += 1
+    print(f'Page request: {lis[i]}')
+    if lis[i] in lista:  # se for um hit
+        hit += 1
+        # dá update na lista de prioridade
         for z in range(tam):
-            if lis[i] != lista[z]:
-                print(f'[{lista[z]}]')
-
-            else:
+            if lista[z] == lis[i]:
+                lista2[z] = cont
+                cont += 1
+        for z in range(tam):
+            if lista[z] == lis[i]:
                 print(f'[{lista[z]}] <- (hit)')
-
-    else: #se der miss
-        miss= miss + 1
-        # if lis[i] not in lista2:
-        #     lista2[cont2] = lis[i]
-        #     if cont2 == tam-2:
-        #         cont2 = 0
-        #     else:
-        #         cont2 += 1
-        for z in range(tam):
-        #     if lis[z] in lista2:
-        #        print(f'[{lista[z]}]')
-        #     else:
-        #        lista[z] = lis[i]
-        #        print(f'[{lista[z]}] <- (miss)')
-            # print(lis[z], lista2)
-            if lis[i] not in lista2 and controle == 0:
-                lista[z] = lis[i]
-                print(f'[{lista[z]}] <- (miss)')
-                controle += 1
             else:
                 print(f'[{lista[z]}]')
-        
-        if lis[i] not in lista2:
-           lista2[cont2] = lis[i]
-           if cont2 == tam-2:
-               cont2 = 0
-           else:
-               cont2 += 1
-        controle = 0
+    else:  # se for um miss
+        miss += 1
+        # encontra qual tem o menor indice na lista 2
+        lru_index = lista2.index(min(lista2))
+        lista[lru_index] = lis[i]  # troca pelo que foi usado menos recentemente
+        lista2[lru_index] = cont
+        cont += 1
+        for z in range(tam):
+            if lista[z] == lis[i]:
+                print(f'[{lista[z]}] <- (miss)')
+            else:
+                print(f'[{lista[z]}]')
+
 print(f'Hit rate: {hit}/{len(lis)}')
 print(f'Miss rate: {miss}/{len(lis)}')
